ad289fca04f38d568a3c8e62b508291e04e66863
NguyenTrungNguyen <nguyen.trung.nguyen@mediastep.com>
2021-02-24 17:08:33 +0700

    add docs

---
+++ docs/precache-manifest.3a5971db2fb925e5d0c88c9003fb29ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1257e6d026a7dc700833e9e75aee1c21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8327a83aecf13fe5ba0f",
+    "url": "/static/css/main.b0548c40.chunk.css"
+  },
+  {
+    "revision": "36694ada20d89a836f62",
+    "url": "/static/js/2.17ac5442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17ac5442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8327a83aecf13fe5ba0f",
+    "url": "/static/js/main.8f893fb0.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
